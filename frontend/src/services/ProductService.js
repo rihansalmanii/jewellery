03748f057789309ffecfabc2238be9bb1c0bb531
rihansalmanii <salmanirihan76@gmail.com>
2026-08-12 01:01:49 +0530

    add and delete product api initialized for admin role at frontend

--- frontend/src/services/ProductService.js
+++ frontend/src/services/ProductService.js
@@ -9,5 +9,17 @@ export const getAllProducts = async () => {
 export const getProductById = async (id) => {
     const { data } = await api.get(`/products/${id}`)
     return data;
+};
+
+export const addProduct = async (formData) => {
+  const { data } = await api.post("/products/add-product")
+
+  return data;
+}
+
+export const deleteProduct = async (id) => {
+  const { data } = await api.delete("/products/delete-product/:id")
+
+  return data;
 }
 
